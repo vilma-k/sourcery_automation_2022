372ed17484b3e3700d7db2f60ffdd166a41c8fc6
Justas.Lauzadis <justas.lauzadis@devbridge.com>
2022-05-02 16:07:13 +0300

    Test each version of application

--- homework/tests/example.spec.js
+++ homework/tests/example.spec.js
@@ -1,15 +1,31 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
 
-test.describe('Concatenate', () => {
-  test('Concatenating 2 and 3 results in 23', async ({ page }) => {
-    await page.goto('https://testsheepnz.github.io/BasicCalculator');
-    await page.selectOption('#selectBuild', { label: 'Prototype'});
-    await page.locator('#number1Field').type('2');
-    await page.locator('#number2Field').type('3');
-    await page.selectOption('#selectOperationDropdown', {label: 'Concatenate'});
-    await page.locator('#calculateButton').click();
+const data = [
+  'Prototype',
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9'
+]
 
-    await expect(page.locator('#numberAnswerField')).toHaveValue('24');
+
+data.forEach(version => {
+  test.describe(version + ': Add', () => {
+    test('Concatenating 2 and 3 results in 23', async ({ page }) => {
+      await page.goto('https://testsheepnz.github.io/BasicCalculator');
+      await page.selectOption('#selectBuild', { label: version});
+      await page.locator('#number1Field').type('2');
+      await page.locator('#number2Field').type('3');
+      await page.selectOption('#selectOperationDropdown', {label: 'Add'});
+      await page.locator('#calculateButton').click();
+  
+      await expect(page.locator('#numberAnswerField')).toHaveValue('5');
+    });
   });
 });
